16418cef77ddaa8fc0fca2a0be3a7e7b42b362ec
gnoul_ <ttluong4869@gmail.com>
2021-10-24 02:23:31 +0700

    Setup backend

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import xss from 'xss-clean';
+import helmet from 'helmet';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
+//* Middlewares
+app.use(express.json());
 app.use(cors());
 app.use(xss());
+app.use(helmet());
 
 app.get('/', (req, res) => {
     res.send('hello world');
