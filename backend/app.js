d97ceef57fc9f457fece9f583c5814802ce472a7
gnoul_ <ttluong4869@gmail.com>
2021-10-24 16:26:32 +0700

    Create some template files

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,12 @@ import expressLimiter from 'express-rate-limit';
 import cors from 'cors';
 import xss from 'xss-clean';
 import helmet from 'helmet';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from './db/connect.js';
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 
 //* Middlewares
 app.set('trust proxy', 1); //! Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
@@ -26,6 +29,7 @@ app.get('/', (req, res) => {
 
 const start = async () => {
     try {
+        await connectDB(process.env.MONGODB_URI);
         app.listen(port, console.log(`Server is listening on port ${port}...`));
     } catch (error) {
         console.log(error);
