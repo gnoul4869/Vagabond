69c8199a1dbfb4690bdcca9d1680127c3d1c6b47
gnoul_ <ttluong4869@gmail.com>
2021-10-25 02:55:22 +0700

    Update auth.controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 import User from '../models/user.model';
-import bcrypt from 'bcryptjs';
+import { StatusCodes } from 'http-status-codes';
 
 const register = async (req, res) => {
     const user = await User.create(req.body);
+    const token = await User.createJWT();
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
