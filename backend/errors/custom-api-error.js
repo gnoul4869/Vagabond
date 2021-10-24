3673d6980be0810b87e3b3675925c1d54b622550
gnoul_ <ttluong4869@gmail.com>
2021-10-25 01:23:02 +0700

    Create custom-api-error

--- backend/errors/custom-api-error.js
+++ backend/errors/custom-api-error.js
@@ -1,5 +1,24 @@
-class CustomAPIError extends Error {
+import { StatusCodes } from 'http-status-codes';
+
+class BadRequestError extends Error {
     constructor(message) {
         super(message);
+        this.statusCode = StatusCodes.BAD_REQUEST;
     }
 }
+
+class AuthenticationError extends Error {
+    constructor(message) {
+        super(message);
+        this.statusCode = StatusCodes.UNAUTHORIZED;
+    }
+}
+
+class NotFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.statusCode = StatusCodes.NOT_FOUND;
+    }
+}
+
+export { BadRequestError, AuthenticationError, NotFoundError };
