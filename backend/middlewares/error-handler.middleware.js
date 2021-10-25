516f26495444d20bb0888f26581a7e18aae821e6
gnoul_ <ttluong4869@gmail.com>
2021-10-26 00:12:33 +0700

    Upgrade error handler middleware

--- backend/middlewares/error-handler.middleware.js
+++ backend/middlewares/error-handler.middleware.js
@@ -6,6 +6,23 @@ const errorHandler = (err, req, res, next) => {
         msg: err.message || 'Something went wrong, please try again later',
     };
 
+    if (err.code && err.code === 11000) {
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+        customError.msg = `Duplicate key value for ${Object.keys(
+            err.keyValue
+        )}`;
+    }
+
+    if (err.name && err.name === 'ValidationError') {
+        customError.statusCode = StatusCodes.BAD_REQUEST;
+        customError.msg = Object.values(err.errors).map((item) => item.message);
+    }
+
+    if (err.name && err.name === 'CastError') {
+        customError.statusCode = StatusCodes.NOT_FOUND;
+        customError.msg = `No item found with id ${err.value}`;
+    }
+
     return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
