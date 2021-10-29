8406f8b1794a7cb857983e6298fb1c3c352bd28b
gnoul_ <ttluong4869@gmail.com>
2021-10-30 01:20:36 +0700

    Relocate files from backend to root and also disable reportWebVitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 import './css/index.css';
 import './css/navbar.css';
@@ -15,4 +15,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-reportWebVitals(console.log);
+// reportWebVitals(console.log);
