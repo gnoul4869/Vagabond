71e35440be0651cf9a7630d07ab49bfbf5d358bb
gnoul_ <ttluong4869@gmail.com>
2021-10-30 13:23:45 +0700

    Add logics for getAllProducts function

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -1,12 +1,13 @@
 import Product from '../models/product.model.js';
-import {
-    BadRequestError,
-    AuthenticationError,
-    NotFoundError,
-} from '../errors/custom-api-error.js';
+import { BadRequestError, AuthenticationError, NotFoundError } from '../errors/custom-api-error.js';
+import { StatusCodes } from 'http-status-codes';
 
 const getAllProducts = async (req, res) => {
-    throw new BadRequestError('This is a test');
+    const products = await Product.find({});
+    if (!products) {
+        throw new NotFoundError('Không tìm thấy sản phẩm nào');
+    }
+    res.status(StatusCodes.OK).json({ products });
 };
 
 export { getAllProducts };
