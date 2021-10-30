cd5989e2e25d1de689020b66bfdc1972bff5d91a
gnoul_ <ttluong4869@gmail.com>
2021-10-30 16:52:44 +0700

    Install react-number-format and add styles for product list

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import NumberFormat from 'react-number-format';
 import { listProducts } from '../actions/productActions';
 import Loading from './Loading';
 
@@ -15,19 +16,42 @@ const ProductList = () => {
     console.log(typeof products);
 
     return (
-        <>
-            {loading ? (
-                <Loading />
-            ) : error ? (
-                <p>{error}</p>
-            ) : (
-                <div>
-                    {products.map((item) => (
-                        <div>{item.name}</div>
-                    ))}
-                </div>
-            )}
-        </>
+        <div class="container-fluid bg-trasparent my-4 p-3">
+            <div class="row row-cols-1 row-cols-xs-2 row-cols-sm-2 row-cols-lg-4 g-3">
+                {loading ? (
+                    <Loading />
+                ) : error ? (
+                    <p>{error}</p>
+                ) : (
+                    products.map((item) => {
+                        return (
+                            <div key={item.id} class="col">
+                                <div class="card h-100 shadow-sm">
+                                    <img src={item.image} class="card-img-top" alt={item.name} />
+                                    <div class="card-body">
+                                        <div class="clearfix mb-3">
+                                            <span class="float-start badge rounded-pill bg-primary">
+                                                {item.brand}
+                                            </span>
+                                            <h5 class="float-end">
+                                                <NumberFormat
+                                                    value={item.price}
+                                                    displayType={'text'}
+                                                    thousandSeparator={'.'}
+                                                    decimalSeparator={','}
+                                                    prefix={'₫'}
+                                                ></NumberFormat>
+                                            </h5>
+                                        </div>
+                                        <h5 class="card-title">{item.name}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })
+                )}
+            </div>
+        </div>
     );
 };
 
