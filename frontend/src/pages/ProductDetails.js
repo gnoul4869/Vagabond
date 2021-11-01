864e6952c4eab46752938610201bd27802b34331
gnoul_ <ttluong4869@gmail.com>
2021-11-01 23:40:51 +0700

    Update styling for product details

--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { detailsProduct } from '../actions/productActions';
+import { FaCartPlus } from 'react-icons/fa';
+import { RiShoppingBag3Fill } from 'react-icons/ri';
 import ErrorPage from './ErrorPage';
 import NumberInput from '../components/NumberInput';
 import RatingStars from '../components/RatingStars';
@@ -35,14 +37,14 @@ const ProductDetails = () => {
                     <div className="container bg-white mt-5">
                         <div className="container p-3">
                             <div className="row h-3">
-                                <div className="col-4 col-md-3 mx-auto">
+                                <div className="col-12 col-md-6 col-lg-5 mx-auto">
                                     <img
                                         src={product.image[0]}
                                         alt={product.name}
                                         className="img-fluid"
                                     />
                                 </div>
-                                <div className="col-8 col-md-6 mx-auto">
+                                <div className="col-12 col-md-6 col-lg-5 mx-auto">
                                     <div className="row">
                                         <div className="product-details-name fs-3">
                                             {product.name}
@@ -75,11 +77,35 @@ const ProductDetails = () => {
                                         </div>
                                     </div>
                                     <div className="row mt-5">
-                                        <NumberInput
-                                            qty={qty}
-                                            setQty={setQty}
-                                            max={product.countInStock}
-                                        />
+                                        <div className="col-auto my-auto text-secondary product-details-label">
+                                            Số lượng
+                                        </div>
+                                        <div className="col-auto">
+                                            <NumberInput
+                                                qty={qty}
+                                                setQty={setQty}
+                                                max={product.countInStock}
+                                            />
+                                        </div>
+                                        <div className="col-auto my-auto text-secondary product-details-label">
+                                            {product.countInStock} sản phẩm có sẵn
+                                        </div>
+                                    </div>
+                                    <div className="row mt-5">
+                                        <div className="col-auto mb-2 mb-xl-0">
+                                            <button
+                                                type="button"
+                                                className="product-details-btn btn-cart"
+                                            >
+                                                <FaCartPlus className="btn-icon" /> Thêm vào giỏ
+                                                hàng
+                                            </button>
+                                        </div>
+                                        <div className="col-auto mb-2 mb-xl-0">
+                                            <button className="product-details-btn btn-buy">
+                                                <RiShoppingBag3Fill className="btn-icon" /> Mua ngay
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
