1f8818f703d2ac9b94699b7df4824c13fe4c9798
gnoul_ <ttluong4869@gmail.com>
2021-11-02 17:38:32 +0700

    Create function to update main image when clicking small images

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
 
 const ProductCarousel = ({ images, name }) => {
     const newImages = images.slice(0, 5);
+    const [imageNo, setImageNo] = useState(0);
+    const [image, setImage] = useState(newImages[imageNo]);
+
+    // console.log(newImages[imageNo]);
+    // console.log(image);
+
+    useEffect(() => {
+        setImage(newImages[imageNo]);
+    }, [newImages, imageNo]);
 
     return (
         <div className="container mb-4 mb-lg-0">
             <div className="row">
-                <img src={images[0]} alt={name} className="product-carousel-img-big" />
+                <img src={image} alt={name} className="product-carousel-img-big" />
             </div>
             <div className="row mt-3 justify-content-center">
                 <div className="col-auto d-flex justify-content-center p-0 ms-3 mb-2 fs-4">
@@ -16,13 +25,16 @@ const ProductCarousel = ({ images, name }) => {
                     </button>
                 </div>
                 <div className="product-carousel-img-container col d-flex justify-content-center flex-wrap">
-                    {newImages.map((image, index) => {
+                    {newImages.map((item, index) => {
                         return (
                             <img
                                 key={index}
-                                src={image}
+                                src={item}
                                 alt={name}
-                                className="product-carousel-img-small me-1 mb-2"
+                                onClick={() => setImageNo(index)}
+                                className={`product-carousel-img-small me-1 mb-2 ${
+                                    index === imageNo && `product-carousel-img-small-active`
+                                }`}
                             />
                         );
                     })}
