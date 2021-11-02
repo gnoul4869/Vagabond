180e87cdf2cf5c00be3f668cb404e46fefec8523
gnoul_ <ttluong4869@gmail.com>
2021-11-02 18:17:09 +0700

    Finish building ProductCarousel

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -2,15 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { MdChevronRight, MdChevronLeft } from 'react-icons/md';
 
 const ProductCarousel = ({ images, name }) => {
-    const newImages = images.slice(0, 5);
+    const [imageArray, setImageArray] = useState(5);
     const [imageNo, setImageNo] = useState(0);
+    const newImages = images.slice(imageArray - 5, imageArray);
     const [image, setImage] = useState(newImages[imageNo]);
 
-    // console.log(newImages[imageNo]);
-    // console.log(image);
+    const imageButtonHandler = (value) => {
+        if (value >= 5 && value <= images.length) {
+            setImageArray(value);
+        }
+    };
 
     useEffect(() => {
         setImage(newImages[imageNo]);
+        console.log('test');
     }, [newImages, imageNo]);
 
     return (
@@ -19,11 +24,18 @@ const ProductCarousel = ({ images, name }) => {
                 <img src={image} alt={name} className="product-carousel-img-big" />
             </div>
             <div className="row mt-3 justify-content-center">
-                <div className="col-auto d-flex justify-content-center p-0 ms-3 mb-2 fs-4">
-                    <button type="button" className="product-carousel-btn p-0">
-                        <MdChevronLeft className="icon" />
-                    </button>
-                </div>
+                {images.length > 5 && (
+                    <div className="col-auto d-flex justify-content-center p-0 ms-3 mb-2 fs-4">
+                        <button
+                            type="button"
+                            className="product-carousel-btn p-0"
+                            onClick={() => imageButtonHandler(imageArray - 1)}
+                        >
+                            <MdChevronLeft className="icon" />
+                        </button>
+                    </div>
+                )}
+
                 <div className="product-carousel-img-container col d-flex justify-content-center flex-wrap">
                     {newImages.map((item, index) => {
                         return (
@@ -31,19 +43,25 @@ const ProductCarousel = ({ images, name }) => {
                                 key={index}
                                 src={item}
                                 alt={name}
-                                onClick={() => setImageNo(index)}
                                 className={`product-carousel-img-small me-1 mb-2 ${
                                     index === imageNo && `product-carousel-img-small-active`
                                 }`}
+                                onClick={() => setImageNo(index)}
                             />
                         );
                     })}
                 </div>
-                <div className="col-auto d-flex justify-content-center p-0 me-3 mb-2 fs-4">
-                    <button type="button" className="product-carousel-btn p-0">
-                        <MdChevronRight className="icon" />
-                    </button>
-                </div>
+                {images.length > 5 && (
+                    <div className="col-auto d-flex justify-content-center p-0 me-3 mb-2 fs-4">
+                        <button
+                            type="button"
+                            className="product-carousel-btn p-0"
+                            onClick={() => imageButtonHandler(imageArray + 1)}
+                        >
+                            <MdChevronRight className="icon" />
+                        </button>
+                    </div>
+                )}
             </div>
         </div>
     );
