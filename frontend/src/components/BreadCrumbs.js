49e50804309338acda8e30c01a04863757bc22ca
gnoul_ <ttluong4869@gmail.com>
2021-11-03 13:31:16 +0700

    Update BreadCrumbs

--- frontend/src/components/BreadCrumbs.js
+++ frontend/src/components/BreadCrumbs.js
@@ -4,23 +4,31 @@ import { Link } from 'react-router-dom';
 const BreadCrumbs = (props) => {
     return (
         <nav aria-label="breadcrumb">
-            <ol class="breadcrumb my-3">
+            <ol class="breadcrumb flex-nowrap my-3">
                 <li class="breadcrumb-item">
                     <Link to="/" className="link-tag text-primary">
                         Home
                     </Link>
                 </li>
                 {Object.entries(props).map(([itemKey, itemValue], index, { length }) => {
-                    if (length - 1 === index) {
+                    if (index === length - 1) {
                         return (
-                            <li key={index} class="breadcrumb-item active" aria-current="page">
-                                <span>{itemValue}</span>
+                            <li
+                                key={index}
+                                class="breadcrumb-item text-truncate active"
+                                aria-current="page"
+                            >
+                                <span>
+                                    {itemValue}
+                                </span>
                             </li>
                         );
                     } else {
                         return (
-                            <li key={index} class="breadcrumb-item">
-                                <span>{itemValue}</span>
+                            <li key={index} class="breadcrumb-item text-truncate">
+                                <span>
+                                    {itemValue}
+                                </span>
                             </li>
                         );
                     }
