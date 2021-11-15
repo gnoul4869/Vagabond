c1834b4652870df854a6d1ee605ca2a804dc09b9
gnoul_ <ttluong4869@gmail.com>
2021-11-16 03:33:29 +0700

    Add addToCart button to ProductList

--- frontend/src/components/product/ProductList.js
+++ frontend/src/components/product/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { listProducts } from '../../redux/actions/productActions';
@@ -8,10 +8,26 @@ import RatingStars from '../RatingStars';
 import ErrorPage from '../../pages/error/ErrorPage';
 import { FaCartPlus } from 'react-icons/fa';
 import { addToCart } from '../../redux/actions/cartActions';
+import AddToCartModal from '../modals/AddToCartModal';
 
 const ProductList = () => {
     const dispatch = useDispatch();
     const { isLoading, products, error } = useSelector((state) => state.productList);
+
+    const isDone = useSelector((state) => state.cart.isDone);
+    const [isModalShown, setIsModalShown] = useState(false);
+
+    useEffect(() => {
+        if (isDone === true) {
+            setIsModalShown(true);
+        }
+        if (isModalShown === true) {
+            const modalTimeout = setTimeout(() => setIsModalShown(false), 2000);
+            return () => {
+                clearTimeout(modalTimeout);
+            };
+        }
+    }, [isDone, isModalShown]);
 
     useEffect(() => {
         dispatch(listProducts());
@@ -22,49 +38,52 @@ const ProductList = () => {
     }
 
     return (
-        <section className="product-list container pt-3">
-            {isLoading ? (
-                <ProductListLoading />
-            ) : (
-                products &&
-                products.map((item) => {
-                    return (
-                        <div key={item._id} className="product-container">
-                            <Link to={`/product/${item._id}`} className="link-inherit">
-                                <div className="product-image">
-                                    <img src={item.images[0]} alt={item.name} />
-                                </div>
-                                <div className="product-name line-clamp-2">{item.name}</div>
-                            </Link>
-
-                            <div className="product-bottom">
-                                <div className="product-price-status-container">
-                                    <span className="product-price">
-                                        <ProductPrice price={item.price} />
-                                    </span>
-                                    <div className="product-status">
-                                        <RatingStars
-                                            rating={item.rating}
-                                            numReviews={item.numReviews}
-                                        />
-                                        <span className="product-num-reviews text-secondary">
-                                            | {`${item.numReviews} lượt đánh giá`}
-                                        </span>
+        <>
+            <section className="product-list container pt-3">
+                {isLoading ? (
+                    <ProductListLoading />
+                ) : (
+                    products &&
+                    products.map((item) => {
+                        return (
+                            <div key={item._id} className="product-container">
+                                <Link to={`/product/${item._id}`} className="link-inherit">
+                                    <div className="product-image">
+                                        <img src={item.images[0]} alt={item.name} />
                                     </div>
+                                    <div className="product-name line-clamp-2">{item.name}</div>
+                                </Link>
+
+                                <div className="product-bottom">
+                                    <div className="product-price-status-container">
+                                        <span className="product-price">
+                                            <ProductPrice price={item.price} />
+                                        </span>
+                                        <div className="product-status">
+                                            <RatingStars
+                                                rating={item.rating}
+                                                numReviews={item.numReviews}
+                                            />
+                                            <span className="product-num-reviews text-secondary">
+                                                | {`${item.numReviews} lượt đánh giá`}
+                                            </span>
+                                        </div>
+                                    </div>
+                                    <button
+                                        type="button"
+                                        className="product-cart-btn"
+                                        onClick={() => dispatch(addToCart(item._id, 1))}
+                                    >
+                                        <FaCartPlus className="icon" />
+                                    </button>
                                 </div>
-                                <button
-                                    type="button"
-                                    className="product-cart-btn"
-                                    onClick={() => dispatch(addToCart(item._id, 1))}
-                                >
-                                    <FaCartPlus className="icon" />
-                                </button>
                             </div>
-                        </div>
-                    );
-                })
-            )}
-        </section>
+                        );
+                    })
+                )}
+            </section>
+            {isModalShown && <AddToCartModal />}
+        </>
     );
 };
 
