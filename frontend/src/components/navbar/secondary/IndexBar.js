06ca1282e1d06d76b4259f41dd542992c2e6b69f
gnoul_ <ttluong4869@gmail.com>
2021-11-20 21:24:13 +0700

    Update IndexBar

--- frontend/src/components/navbar/secondary/IndexBar.js
+++ frontend/src/components/navbar/secondary/IndexBar.js
@@ -8,16 +8,16 @@ import SearchBox from '../SearchBox';
 const IndexBar = ({ isDropdownShown, setIsDropdownShown }) => {
     return (
         <section>
-            <div className="row d-flex align-items-center justify-content-around pb-3 pt-3 pt-md-0 g-0">
+            <div className="row d-flex align-items-center justify-content-between justify-content-md-around pb-2 pt-3 pt-md-0 g-0">
                 <div className="col-auto d-none d-md-block">
                     <Brand />
                 </div>
 
-                <div className="col-2 d-flex d-md-none justify-content-center">
+                <div className="col-auto d-flex d-md-none">
                     <Logo />
                 </div>
 
-                <div className="col col-md-7 col-lg-8">
+                <div className="col-auto col-md-7 col-lg-8">
                     <SearchBox />
                 </div>
 
