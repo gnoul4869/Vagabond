eb82527682855825bbfe589b469929d0ef9f0154
gnoul_ <ttluong4869@gmail.com>
2021-11-21 20:56:58 +0700

    Create ShippingDetails

---
+++ frontend/src/components/checkout/ShippingDetails.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { MdPlace } from 'react-icons/md';
+
+const ShippingDetails = ({ name, address, phoneNumber }) => {
+    return (
+        <div className="mt-3 bg-white">
+            <div className="postcard-border"></div>
+            <div className="container p-4">
+                <div className="text-ired fw-600 fs-5">
+                    <MdPlace className="icon me-1" />
+                    Thông tin đặt hàng
+                </div>
+                <div className="container px-3">
+                    <div className="row mt-3">
+                        <div className="col-12 col-md">
+                            <div className="row">
+                                <div className="col-4 col-md-3 col-lg-4 text-secondary">
+                                    <span className="d-none d-md-flex">Tên người nhận</span>
+                                    <span className="d-flex d-md-none">Tên</span>
+                                </div>
+                                <div className="col p-0">
+                                    <span>{name}</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-12 col-md mt-3 mt-md-0">
+                            <div className="row">
+                                <div className="col-4 col-md-3 col-lg-4 text-secondary">
+                                    <span className="d-none d-md-flex">Số điện thoại</span>
+                                    <span className="d-flex d-md-none">Sđt</span>
+                                </div>
+                                <div className="col p-0">
+                                    <span>{phoneNumber}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mt-3">
+                        <div className="col-4 col-md-3 col-lg-2 text-secondary">
+                            <span className="d-none d-md-flex">Địa chỉ nhận hàng</span>
+                            <span className="d-flex d-md-none">Địa chỉ</span>
+                        </div>
+                        <div className="col p-0">
+                            <span>{address}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ShippingDetails;
--- frontend/src/pages/CheckoutPage.js
+++ frontend/src/pages/CheckoutPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import { MdPlace } from 'react-icons/md';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useLocation } from 'react-router';
+import ShippingDetails from '../components/checkout/ShippingDetails';
 import CheckoutLoading from '../components/loading/CheckoutLoading';
 import { getUserDetails } from '../redux/actions/userAction';
 
@@ -33,54 +33,11 @@ const CheckoutPage = () => {
                 <CheckoutLoading />
             ) : (
                 userDetails && (
-                    <div className="mt-3 bg-white">
-                        <div className="postcard-border"></div>
-                        <div className="container p-4">
-                            <div className="text-ired fw-600 fs-5">
-                                <MdPlace className="icon me-1" />
-                                Thông tin đặt hàng
-                            </div>
-                            <div className="container px-3">
-                                <div className="row mt-3">
-                                    <div className="col-12 col-md">
-                                        <div className="row">
-                                            <div className="col-4 col-md-3 col-lg-4 text-secondary">
-                                                <span className="d-none d-md-flex">
-                                                    Tên người nhận
-                                                </span>
-                                                <span className="d-flex d-md-none">Tên</span>
-                                            </div>
-                                            <div className="col p-0">
-                                                <span>{userDetails.name}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-12 col-md mt-3 mt-md-0">
-                                        <div className="row">
-                                            <div className="col-4 col-md-3 col-lg-4 text-secondary">
-                                                <span className="d-none d-md-flex">
-                                                    Số điện thoại
-                                                </span>
-                                                <span className="d-flex d-md-none">Sđt</span>
-                                            </div>
-                                            <div className="col p-0">
-                                                <span>{userDetails.phoneNumber}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="row mt-3">
-                                    <div className="col-4 col-md-3 col-lg-2 text-secondary">
-                                        <span className="d-none d-md-flex">Địa chỉ nhận hàng</span>
-                                        <span className="d-flex d-md-none">Địa chỉ</span>
-                                    </div>
-                                    <div className="col p-0">
-                                        <span>{userDetails.address}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <ShippingDetails
+                        name={userDetails.name}
+                        address={userDetails.address}
+                        phoneNumber={userDetails.phoneNumber}
+                    />
                 )
             )}
         </>
