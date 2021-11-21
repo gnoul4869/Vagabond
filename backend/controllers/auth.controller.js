4c9a9c37acd7dff0a7b16fdeb8099421de51e2c7
gnoul_ <ttluong4869@gmail.com>
2021-11-22 05:25:22 +0700

    Update Auth Controller and Address Model

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,9 +2,24 @@ import User from '../models/user.model.js';
 import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, AuthenticationError } from '../errors/custom-api-error.js';
 import moment from 'moment';
+import Address from '../models/address.model.js';
 
 export const register = async (req, res) => {
-    const { email, password, name, address, phoneNumber, gender, birthDate } = req.body;
+    const {
+        email,
+        password,
+        name,
+        phoneNumber,
+        gender,
+        birthDate,
+        provinceID,
+        provinceName,
+        districtID,
+        districtName,
+        wardID,
+        wardName,
+        addressDetails,
+    } = req.body;
 
     if (!email) {
         throw new BadRequestError('Hãy nhập email của bạn');
@@ -50,10 +65,6 @@ export const register = async (req, res) => {
         throw new BadRequestError('Tên không thể có nhiều hơn 40 ký tự');
     }
 
-    if (!address) {
-        throw new BadRequestError('Hãy nhập địa chỉ của bạn');
-    }
-
     if (!phoneNumber) {
         throw new BadRequestError('Hãy nhập số điện thoại của bạn');
     }
@@ -86,7 +97,45 @@ export const register = async (req, res) => {
         throw new BadRequestError('Số tuổi không hợp lệ');
     }
 
-    const user = await User.create(req.body);
+    if (!provinceID) {
+        throw new BadRequestError('Hãy cung cấp mã tỉnh/thành phố');
+    }
+
+    if (!provinceName) {
+        throw new BadRequestError('Hãy cung cấp tên tỉnh/thành phố');
+    }
+
+    if (!districtID) {
+        throw new BadRequestError('Hãy cung cấp mã quận/huyện');
+    }
+
+    if (!districtName) {
+        throw new BadRequestError('Hãy cung cấp tên quận/huyện');
+    }
+
+    if (!wardID) {
+        throw new BadRequestError('Hãy cung cấp mã phường/xã');
+    }
+
+    if (!wardName) {
+        throw new BadRequestError('Hãy cung cấp tên phường/xã');
+    }
+
+    if (!addressDetails) {
+        throw new BadRequestError('Hãy cung cấp địa chỉ cụ thể');
+    }
+
+    const user = await User.create({ email, password, name, phoneNumber, gender, birthDate });
+    const address = await Address.create({
+        provinceID,
+        provinceName,
+        districtID,
+        districtName,
+        wardID,
+        wardName,
+        details: addressDetails,
+        createdBy: user._id,
+    });
     const token = await user.createJWT();
     res.status(StatusCodes.CREATED).json({
         userInfo: {
@@ -96,6 +145,13 @@ export const register = async (req, res) => {
             phoneNumber: user.phoneNumber,
             gender: user.gender,
             birthDate: user.birthDate,
+            provinceID: address.provinceID,
+            provinceName: address.provinceName,
+            districtID: address.districtID,
+            districtName: address.districtName,
+            wardID: address.wardID,
+            wardName: address.wardName,
+            addressDetails: address.details,
             role: user.role,
             image: user.image,
             token: token,
@@ -120,6 +176,8 @@ export const login = async (req, res) => {
         throw new AuthenticationError('Email hoặc mật khẩu của bạn không đúng');
     }
 
+    const address = await Address.findOne({ createdBy: user._id });
+
     const token = user.createJWT();
 
     res.status(StatusCodes.OK).json({
@@ -130,6 +188,13 @@ export const login = async (req, res) => {
             phoneNumber: user.phoneNumber,
             gender: user.gender,
             birthDate: user.birthDate,
+            provinceID: address.provinceID,
+            provinceName: address.provinceName,
+            districtID: address.districtID,
+            districtName: address.districtName,
+            wardID: address.wardID,
+            wardName: address.wardName,
+            addressDetails: address.details,
             role: user.role,
             image: user.image,
             token: token,
