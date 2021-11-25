13bb2177d11e2027fafd3f22bc4588879188b6a6
gnoul_ <ttluong4869@gmail.com>
2021-11-25 10:02:54 +0700

    Create getUserOrders and change getAllOrders for admin only

--- backend/controllers/order.controller.js
+++ backend/controllers/order.controller.js
@@ -1,5 +1,5 @@
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError, NotFoundError } from '../errors/custom-api-error.js';
+import { AuthenticationError, BadRequestError, NotFoundError } from '../errors/custom-api-error.js';
 import Order from '../models/order.model.js';
 
 export const createOrder = async (req, res) => {
@@ -18,7 +18,23 @@ export const createOrder = async (req, res) => {
     res.status(StatusCodes.CREATED).json({ order });
 };
 
+export const getUserOrders = async (req, res) => {
+    const { status } = req.query;
+    const orders = status
+        ? await Order.find({ createdBy: req.user.id, status })
+        : await Order.find({ createdBy: req.user.id }).sort({ createdAt: 'desc' });
+
+    if (!orders) {
+        throw new NotFoundError('Không tìm thấy đơn hàng nào');
+    }
+
+    res.status(StatusCodes.OK).json({ orders });
+};
+
 export const getAllOrders = async (req, res) => {
+    if (req.user.role !== 'admin') {
+        throw new AuthenticationError('Không đủ quyền thực hiện');
+    }
     const { status } = req.query;
     const orders = status
         ? await Order.find({ status })
--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { createOrder, getAllOrders } from '../controllers/order.controller.js';
+import { createOrder, getAllOrders, getUserOrders } from '../controllers/order.controller.js';
 const router = express.Router();
 
-router.route('/').post(createOrder).get(getAllOrders);
+router.route('/').post(createOrder).get(getUserOrders);
+router.get('/all', getAllOrders);
 
 export default router;
