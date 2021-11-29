012b05498bc2f0eba048f365055bb4eb39f82d69
gnoul_ <ttluong4869@gmail.com>
2021-11-29 12:27:41 +0700

    Create Electronic and Mobile

---
+++ backend/db/products/electronic.js
@@ -0,0 +1,75 @@
+export const electronic = [
+    {
+        name: 'Android Tivi Sony 4K 43 inch KD-43X75A',
+        category: 'Điện tử',
+        price: 12800000,
+        countInStock: 400,
+        brand: 'Sony',
+        weight: 11000,
+        description:
+            'Tính năng kỹ thuật</br></br>Thiết kế:</br></br>- Khung viền nhựa, vân nét mảnh</br></br>- Chân đế thiết kế vát mỏng, thanh lịch</br></br></br></br>Công nghệ hình ảnh</br></br>- Màn hình sử dụng công nghệ LED nền (Direct LED, Frame dimming)</br></br>- Bộ xử lý X1 4K Processor cho hình ảnh trung thực, sắc nét</br></br>- Độ phân giải 4K, chi tiết gấp 4 lần so với TV Full HD</br></br>- Công nghệ 4K X-Reality PRO tự động nâng cấp hình ảnh lên 4K</br></br>- Công nghệ chuyển động XR100, Auto mode cho hình ảnh chuyển động nhanh mượt mà</br></br>- Hỗ trợ HDR10, HLG, Dolby Vision</br></br>Công nghệ âm thanh</br></br>- Công suất loa 20W (10W + 10W), 2.0 kênh</br></br>- Hỗ trợ DTS Digital Surround, S-Master Digital Amplifier, S-Force Front Surround</br></br>- Cho phép tùy chọn âm thanh theo nội dung xem phim, ca nhạc, bóng đá… </br></br>- Two channel linear PCM: 48kHz 16bits, Dolby Audio, DTS </br></br>Hệ điều hành và tiện ích</br></br>- TV hệ điều hành Android 10. </br></br>- Google Play với hơn 5,000 ứng dụng giải trí phong phú tiếng Việt và tiếng Anh.</br></br>- Tích hợp trợ lý ảo Google Assistant</br></br>- Tìm kiếm giọng nói tiếng Việt 3 miền với remote RMF-TX520P</br></br>- Ứng dụng Chromecast dễ dàng chia sẻ nội dung từ điện thoại di động, máy tính bảng (Android) lên màn hình TV</br></br>- Tích hợp Bộ giải mã Truyền hình kỹ thuật số DVB-T2.</br></br>Kết nối</br></br>- Kết nối Wi-fi (tích hợp) 2 băng tần và cổng LAN .</br></br>- Kết nối Bluetooth với chuột, bàn phím, tay cầm chơi game và 1 số tai nghe, loa của Sony</br></br>- Kết nối USB (2 cổng), kết nối HDMI (3 cổng).',
+        images: [
+            'https://i.ibb.co/3SNtgw7/android-tivi-sony-4k-43inch-kd-43x75a-01.jpg',
+            'https://i.ibb.co/6YgpkB3/android-tivi-sony-4k-43inch-kd-43x75a-02.jpg',
+            'https://i.ibb.co/QjwKKHs/android-tivi-sony-4k-43inch-kd-43x75a-03.jpg',
+        ],
+    },
+    {
+        name: ' Loa bluetooth ANKER SoundCore Stereo - A3102',
+        category: 'Điện tử',
+        price: 1200000,
+        countInStock: 220,
+        brand: 'ANKER',
+        weight: 2200,
+        description:
+            'Sản phẩm có thời gian chơi nhạc lâu nhất trong ngành công nghiệp loa di động, với một kích cỡ nhỏ nhất. Mang đến trải nghiệm âm thanh di động đích thực  </br></br>Âm Thanh Chất Lượng Cao: Cho dù bạn đang tiệc tùng, làm việc, hay chỉ là thơ thẩn quanh nhà, thưởng thức một âm thanh stereo tuyệt vời với chất lượng âm thanh ấn tượng</br></br</br></br>Âm Bass Ấn Tượng: Để giữ SoundCore nhỏ gọn nhưng vẫn đảm bảo chất lượng âm thanh, chúng tôi đã thiết kế nó bằng cách sử dụng thiết kế cổng bass xoắn ốc độc Kết quả là </br></br>một chiếc loa nhỏ gọn nhưng có âm bass ấn tượng</br></br</br></br>Kết Nối Bluetooth 4.0: Không giống như các loa khác, Anker sử dụng công nghệ Bluetooth mới nhất để tối đa hóa khả năng tương thích thiết bị và tốc độ kết nối</br></br</br></br>Thời lượng pin lâu gấp 2 lần: Công nghệ quản lý năng lượng mới của Anker cho phép Loa SoundCore chơi nhạc liên tục 24 giờ (Với âm lượng 60%). Nghe được 500hát, xem </br></br>được 15 bộ phim liên tiếp</br></br</br></br>Dễ dàng mang theo: Với kích thước nhỏ gọn, bạn có thể mang theo khi đi làm, đi du lịch hoặc chỉ là nghe nhạc trên ô tô.',
+        images: [
+            'https://i.ibb.co/VQGHXcv/anker-soundcore-stereo-a3102-01.jpg',
+            'https://i.ibb.co/Yj9qvr4/anker-soundcore-stereo-a3102-02.jpg',
+            'https://i.ibb.co/NS9L2LT/anker-soundcore-stereo-a3102-03.jpg',
+        ],
+    },
+    {
+        name: 'Tay Cầm PS5 Sony DualSense Controller Play Station 5',
+        category: 'Điện tử',
+        price: 1200000,
+        countInStock: 120,
+        brand: 'Sony',
+        weight: 1500,
+        description:
+            'Bộ điều khiển không dây DualSense cho PS5 cung cấp bạn đắm chìm trong xúc giác chân thật  (*), bộ kích hoạt thích ứng động (**) và micrô tích hợp, tất cả đều được tích hợp vào một thiết kế mang tính biểu tượng.</br></br>(*) Cảm nhận phản hồi nhạy bén về mặt vật lý đối với các hành động trong trò chơi của bạn với bộ truyền động kép thay thế động cơ rung truyền thống. Trong bạn, những </br></br>rung động năng động này có thể mô phỏng cảm giác của mọi thứ từ môi trường đến độ giật của các loại vũ khí khác nhau.</br></br>(**) Trải nghiệm các mức độ lực và độ căng khác nhau khi bạn tương tác với các thiết bị và môi trường trong trò chơi của mình. Từ việc kéo căng dây cung đến nphanh </br></br>trên một chiếc xe đang chạy tốc độ cao, đây là những tính năng đặc biệt không thể tìm thấy trên các tay cầm khác',
+        images: [
+            'https://i.ibb.co/GV7283X/ps5-controller-01.jpg',
+            'https://i.ibb.co/z52XKZ9/ps5-controller-02.jpg',
+            'https://i.ibb.co/DpT1mzT/ps5-controller-03.png',
+        ],
+    },
+    {
+        name: 'Tủ lạnh Sharp Inverter 626 lít SJ-FX631V-SL',
+        category: 'Điện tử',
+        price: 16500000,
+        countInStock: 60,
+        brand: 'Sharp',
+        weight: 100000,
+        description:
+            'Thông số kỹ thuật chi tiết Tủ lạnh Sharp Inverter 626 lít SJ-FX630V-SL</br></br>Đặc điểm sản phẩm</br></br>Dung tích tổng:626 lít</br></br>Dung tích sử dụng:556 lít</br></br>Số người sử dụng:4 - 5 người</br></br>Dung tích ngăn đá:211 lít</br></br>Dung tích ngăn lạnh:345 lít</br></br>Công nghệ Inverter:Tủ lạnh Inverter</br></br>Công nghệ làm lạnh:Hệ thống làm lạnh kép Hybrid Cooling</br></br>Công nghệ kháng khuẩn, khử mùi:Bộ lọc với các phân tử Ag+Cu</br></br>Công nghệ bảo quản thực phẩm:Ngăn rau quả giữ ẩm</br></br>Tiện ích:Chuông báo cửa mở, Bảng điều khiển bên ngoài</br></br>Kiểu tủ:Tủ lớn - Side by side</br></br>Chất liệu cửa tủ lạnh:Thép không gỉ</br></br>Chất liệu khay ngăn lạnh:Kính chịu lực',
+        images: [
+            'https://i.ibb.co/80KjjmZ/sharp-inverter-sj-fx631-vsl-01.jpg',
+            ' https://i.ibb.co/Bt05w13/sharp-inverter-sj-fx631-vsl-02.jpg',
+        ],
+    },
+    {
+        name: 'Smart Tivi Netflix 4K UHD Coocaa 55 inch Wifi - Model 55S3N',
+        category: 'Điện tử',
+        price: 8600000,
+        countInStock: 300,
+        brand: 'Coocaa',
+        weight: 13000,
+        description:
+            'Smart TV tích hợp Netflix 5.1 </br></br>Ultra HD (3840x2160)</br></br>4k HDR-10 & HLG Supported</br></br>Công nghệ FOCL (Tràn viền vô cực- mở rộng 97% hình ảnh hiển thị)</br></br>Âm thanh Dolby với DTS TruSurround</br></br>HDMI1.4 * 3; USB2.0 *2 AV-in SPIDIF out RF Wifi Digital TV</br></br>Digital TV DVB T/T2, Wifi & Lan</br></br>Youtube app/ Open Browsers/ Live TV</br></br></br></br>Tivi Netflix 4K UHD Coocaa 55 inch Wifi - Tràn Viền - Model 55S3N - Miễn phí lắp đặt</br></br>_ Smart TV, Netflix built-in luôn hợp xu hướng</br></br>_ Nội dung Netflix cao cấp theo yêu cầu, với Youtube - thế giới gần bạn hơn.</br></br>_ Duyệt những gì bạn muốn</br></br>Với một cú click, bạn có thể truy cập các trang yêu thích bao gồm Facebook, Wikipedia, Instagram và nhiều hơn thế nữa.</br></br>_ Phát theo cách bạn muốn</br></br>Chỉ cần click vào biểu tượng phát trên điện thoại của bạn để truyền video và hình ảnh lên TV và có được trải nghiệm xem tốt hơn.</br></br>* Hãy chắc chắc rằng TV và điện thoại của bạn kết nối dưới cùng thiết bị wifi.</br></br>* Hỗ trợ chia sẻ màn hình.</br></br>_ Xem rõ hơn, chất lượng tuyệt vời</br></br>_ Thiết kế thời thượng</br></br>Kết cấu đáy viền bằng chất liệu da </br></br>_ Khung hình tràn viền cho cái nhìn vô cực</br></br>Tỉ lệ màn hình so với thân máy là 97%. TV coocaa 55S3N sử dụng công nghệ FOCL để phân giải các khung hình trên/trái và phải để tối ưu không gian giải trí của đình bạn, </br></br>trong khi 55 inch thông thường có tỉ lệ màn hình so với thân máy là 94%.</br></br>* Frameless Open-cell Lock-in Technology</br></br>_ Mở rộng thế giới quan của bạn </br></br>Tivi Coocaa 55 inch 55S3N sở hữu độ phân giải 4K cho chất lượng hình ảnh trung thực nhất khi hiển thị trên màn hình.</br></br>_ Điều chỉnh để hoàn thiện</br></br>Lắng nghe - Cảm nhận - Thấu hiểu - Kết nối</br></br>* Powerful Stereo Speaker</br></br>_ Đa kết nối - Đa phương tiện </br></br>Sử dụng USB để phát phim hoặc nhạc ở nhiều định dạng, kết nối HDMI để mở rộng màn hình máy tính, kết nối loa ngoài để thưởng thức âm thanh vòm.</br></br>Các cổng: HDMI x 3; S/PDIF out; AV INPUT; USB x 2; LAN.</br></br>Bộ sản phẩm đầy đủ:</br></br>2 x Chân đế, 1 x Remote, 1 x Sách HDSD, 1 x Dây nguồn.',
+        images: [
+            'https://i.ibb.co/jvh118V/smart-tivi-netflix-4k-uhd-coocaa-55inch-wifi-model-55s3n-01.jpg',
+            'https://i.ibb.co/hRZMKxy/smart-tivi-netflix-4k-uhd-coocaa-55inch-wifi-model-55s3n-02.jpg',
+        ],
+    },
+];
---
+++ backend/db/products/mobile.js
@@ -0,0 +1,72 @@
+export const mobile = [
+    {
+        name: 'Apple iPhone 13 128GB',
+        category: 'Điện thoại',
+        price: 25000000,
+        countInStock: 200,
+        brand: 'Apple',
+        weight: 2000,
+        description:
+            'iPhone 13. Hệ thống camera kép tiên tiến nhất từng có trên iPhone. Chip A15 Bionic thần tốc. Bước nhảy vọt về thời lượng pin. Thiết kế bền bỉ. Mạng 5G siêu nhanh. Cùng với màn hình Super Retina XDR sáng hơn.',
+        images: [
+            'https://i.ibb.co/phHsDdt/apple-iphone-13-128gb-01.png',
+            'https://i.ibb.co/CB0jQkW/apple-iphone-13-128gb-02.jpg',
+        ],
+    },
+    {
+        name: 'OPPO A74 (8GB/128GB)',
+        category: 'Điện thoại',
+        price: 6500000,
+        countInStock: 130,
+        brand: 'OPPO',
+        weight: 2300,
+        description:
+            'iPhone 13. HệThiết kế quen thuộc, màu sắc tối giản</br></br>Chiếc điện thoại OPPO A74 vẫn sở hữu cụm camera nằm trong mô-đun hình chữ nhật góc trái thân thuộc của hãng, máy được gia công tỉ mỉ, tối ưu kích thước gsmartphone </br></br>mỏng nhẹ, thiết kế thân máy cong 3D mảnh mai, dễ cầm nắm sử dụng.</br></br></br></br>Tỏa sáng sắc nét với màn hình AMOLED </br></br>OPPO A74 sở hữu màn hình có kích thước hiển thị khá rộng 6.43 inch với tỷ lệ màn hình so với thân máy lên đến 90.8% cho không gian hiển thị rộng mở, cho tnghiệm người </br></br>dùng thêm sự rõ ràng, đã mắt.</br></br></br></br>Độ sáng tiêu chuẩn 430 nits và độ sáng tối đa 800 nits cho khả năng hiển thị tốt nội dung ở mọi môi trường ánh sáng, hơn nữa máy còn có được trang bị các biến thông </br></br>minh, liên tục kiểm tra và điều chỉnh mức độ sáng phù hợp cho đôi mắt của bạn.</br></br></br></br>Sẵn sàng chụp ảnh đẹp mọi lúc mọi nơi</br></br>OPPO đã trang bị cho chiếc smartphone A74 của mình một cụm 3 camera sau gồm: Camera chính có độ phân giải 48 MP, camera macro 2 MP và cuối cùng là camera đo độ sâu 2 MP.</br></br></br></br>Bên cạnh đó, máy ảnh selfie hỗ trợ độ phân giải 16 MP với các hiệu ứng AI làm đẹp được tuỳ chỉnh dễ dàng mang lại vẻ đẹp thực sự cho bạn, mang đến cho bạn sựtin hơn </br></br>trước ống kính.</br></br></br></br>Hiệu năng ổn định, mượt mà </br></br>OPPO A74 trang bị chipset Snapdragon 662 được sản xuất dựa trên tiến trình 11 nm, có tốc độ xử lý 2.0 GHz với các tác vụ thông thường máy hoàn toàn đáp ứng mang đến </br></br>những trải nghiệm cao cấp cho bạn.</br></br></br></br>OPPO A74 hoạt động và xử lý dữ liệu nhanh chóng, đa nhiệm mượt mà với RAM 8 GB và bạn có thể tận hưởng một bộ nhớ dồi dào, rộng rãi với 128 GB bộ nhớ trong. nhu cầu sử </br></br>dụng cao hơn, OPPO A74 vẫn hỗ trợ thẻ nhớ ngoài MicroSD cho bạn yên tâm hơn khi sử dụng. thống camera kép tiên tiến nhất từng có trên iPhone. Chip Bionic thần tốc. </br></br>Bước nhảy vọt về thời lượng pin. Thiết kế bền bỉ. Mạng 5G siêu nhanh. Cùng với màn hình Super Retina XDR sáng hơn.',
+        images: [
+            'https://i.ibb.co/6PskP97/oppo-a74-8gb-128gb-01.jpg',
+            'https://i.ibb.co/LrqRNZF/oppo-a74-8gb-128gb-02.jpg',
+        ],
+    },
+    {
+        name: 'Realme C21Y 4GB',
+        category: 'Điện thoại',
+        price: 3650000,
+        countInStock: 100,
+        brand: 'Realme',
+        weight: 2100,
+        description:
+            'Thiết kế trẻ trung, năng động </br></br>Realme C21Y sở hữu ngôn ngữ thiết kế trẻ trung, năng động với các họa tiết hình học đan chéo được cắt vát một cách tinh xảo cùng hiệu ứng màu sắc bắt mắt, tạotượng lớn </br></br>cho người dùng ngay từ cái nhìn đầu tiên.</br></br>Màn hình giọt nước tràn viền thời trang </br></br>Realme C21Y được trang bị màn hình tràn viền được thiết kế dạng “giọt nước” thời thượng với kích thước lớn lên tới 6.5 inch mang đến khung hình cực rộng gngười dùng có </br></br>thể thoải mái trải nghiệm những chương trình giải trí, xem phim trên điện thoại. </br></br>Bộ vi xử lý 8 nhân cho hiệu năng ổn định</br></br>Realme C21Y sở hữu bộ vi xử lý Spreadtrum T610 8 nhân, đáp ứng tốt nhu cầu giải trí hằng ngày như đọc báo, xem phim hay chơi tốt các tựa game nhẹ nhàng.</br></br>Cụm camera “bếp từ” nổi bật</br></br>Realme C21Y được trang bị cụm camera "bếp từ" trông khá hoành tráng ở phía sau với 3 ống kính bao gồm chiếc camera chính 13 MP, ống kính B&W 2 MP hỗ trợ chụp phông và </br></br>camera 2 MP chụp macro.</br></br>Dung lượng pin thoải mái sử dụng cả ngày</br></br>Điểm nhấn trên Realme C21Y chính là dung lượng pin lớn lên tới 5000 mAh cho bạn thoải mái có thể sử dụng cả ngày mà không phải quá lo lắng bị hết pin làm gđoạn cuộc </br></br>vui giữa chừng.',
+        images: [
+            'https://i.ibb.co/wwxdQk0/Realme-C21-Y-4-GB-01.jpg',
+            'https://i.ibb.co/rkWsLL7/Realme-C21-Y-4-GB-02.jpg',
+        ],
+    },
+    {
+        name: 'Samsung Galaxy M22 (6GB/128GB)',
+        category: 'Điện thoại',
+        price: 4800000,
+        countInStock: 400,
+        brand: 'Samsung',
+        weight: 2200,
+        description:
+            '1. Màn hình        </br></br>Công nghệ màn hình: AMOLED</br></br>Độ phân giải: HD+ (720 x 1600 Pixels)</br></br>Màn hình rộng: 6.4" - Tần số quét 90 Hz</br></br>2. Camera sau        </br></br>Độ phân giải: Chính 48 MP & Phụ 8 MP, 2 MP, 2 MP</br></br>Đèn Flash: Có</br></br>3. Camera trước        </br></br>Độ phân giải: 13 MP</br></br>Tính năng: HDR Quay video Full HD</br></br>4. Hệ điều hành & CPU        </br></br>Hệ điều hành: Android 11</br></br>Chip xử lý (CPU): MediaTek Helio G80 8 nhân</br></br>Tốc độ CPU: 2 nhân 2.0 GHz & 6 nhân 1.8 GHz</br></br>Chip đồ họa (GPU): Mali-G52 MC2</br></br>5. Bộ nhớ & Lưu trữ</br></br>RAM: 6 GB</br></br>Bộ nhớ trong: 128 GB</br></br>6. Danh bạ: Không giới hạn</br></br>7. Kết nối        </br></br>Mạng di động: Hỗ trợ 4G</br></br>Cổng kết nối/sạc: Type-C</br></br>Jack tai nghe: 3.5 mm</br></br>8. Pin & Sạc        </br></br>Dung lượng pin: 5000 mAh</br></br>Loại pin:   Li-Po</br></br>Hỗ trợ sạc tối đa: 25 W</br></br>9. Tiện ích   </br></br>Bảo mật nâng cao:    Mở khoá vân tay cạnh viền     </br></br>Radio: Đang cập nhật</br></br>Xem phim: Có        </br></br>Nghe nhạc: Có',
+        images: [
+            'https://i.ibb.co/0qqD1bx/samsung-galaxy-m22-6gb-128gb-01.jpg',
+            'https://i.ibb.co/8578qqX/samsung-galaxy-m22-6gb-128gb-02.jpg',
+        ],
+    },
+    {
+        name: 'Xiaomi Redmi 9C 4GB/128GB',
+        category: 'Điện thoại',
+        price: 3500000,
+        countInStock: 300,
+        brand: 'Xiaomi',
+        weight: 2100,
+        description:
+            'Điện Thoại Xiaomi Redmi 9C 4GB/128GB - Hàng Chính Hãng</br></br>Bộ sản phẩm bao gồm: Pin, sạc, cáp USB, cây tháo sim, sách hướng dẫn.</br></br>Thông số kỹ thuật</br></br></br></br>Màn hình: IPS LCD, 6.53", HD+</br></br></br></br>Hệ điều hành:	Android 10</br></br></br></br>Camera sau: Chính 13 MP & Phụ 2 MP, 2 MP</br></br></br></br>Camera trước: 5 MP</br></br></br></br>CPU: MediaTek Helio G35 8 nhân</br></br></br></br>RAM: 4 GB</br></br></br></br>Bộ nhớ trong: 128 GB</br></br></br></br>Thẻ nhớ:	MicroSD, hỗ trợ tối đa 512 GB</br></br></br></br>Dung lượng pin: 5000 mAh</br></br>Điện Thoại Xiaomi Redmi 9C 4GB/128GB - Hàng Chính Hãng',
+        images: [
+            'https://i.ibb.co/MNhg48Q/xiaomi-redmi-9c-4gb-128-gb-01.jpg',
+            'https://i.ibb.co/9hw1QvZ/xiaomi-redmi-9c-4gb-128-gb-02.jpg',
+        ],
+    },
+];
