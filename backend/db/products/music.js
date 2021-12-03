2a35dbf7c89481107f7c644fb9c83eebf638c4f9
gnoul_ <ttluong4869@gmail.com>
2021-12-03 20:43:05 +0700

    Add 5 more music products to db

--- backend/db/products/music.js
+++ backend/db/products/music.js
@@ -1,6 +1,6 @@
 export const music = [
     {
-        name: 'DÂY GUITAR CLASSIC CAO CẤP AC139 ALICE CHÍNH HÃNG',
+        name: 'Dây guitar classic Alice AC139',
         category: 'Âm nhạc',
         price: 130000,
         countInStock: 200,
@@ -15,15 +15,41 @@ export const music = [
         ],
     },
     {
+        name: 'Đàn Guitar Acoustic Rosen G15 Gỗ Mahogany',
+        category: 'Âm nhạc',
+        price: 1749000,
+        countInStock: 50,
+        brand: 'Rosen',
+        weight: 3400,
+        description:
+            'Dòng guitar Rosen từ lâu đã được khẳng định về giá trị và độ phổ biến trên thị trường đàn Acoustic tại Việt Nam. Rosen Acoustic được sản xuất với mẫu mã đẹp, chất lượng gỗ và thi công từng đường nét chuẩn sắc sảo để cho ra âm thanh hay, vang và sáng.\n\nGuitar Rosen là một dòng G thuộc phân khúc cao của hãng Rosen có nhiều đặc điểm vượt trội so với các dòng G11, G12, R125, R135 về chất gỗ cũng như âm thanh.',
+        images: [
+            'https://i.ibb.co/SxmhkrS/ascoustic-rosen-g15-01.jpg',
+            'https://i.ibb.co/Q69VjP7/ascoustic-rosen-g15-02.jpg',
+            'https://i.ibb.co/nb3hbz4/ascoustic-rosen-g15-03.jpg',
+        ],
+    },
+    {
         name: 'Capo Gỗ Chất Lượng Cho Đàn Guitar Acoustic và Classic',
         category: 'Âm nhạc',
         price: 55000,
         countInStock: 200,
         brand: 'Noname',
-        weight: 250,
+        weight: 150,
         description:
             'Capo Gỗ Siêu Xịn Dành cho đàn guitar\n\nCapo màu vân gỗ siêu bền với lò xi cực kỳ chắc chắn\n\nCapo chuyên dụng cho đàn guitar acoustic và có thể sử dụng cho đàn guitar classic\n\nCapo vân gỗ có màu gỗ sáng và tối\n\nHàng chất lượng chính hãng 100%',
         images: ['https://i.ibb.co/vhfWqww/capo-go.jpg', 'https://i.ibb.co/L1vYGrc/capo-go-02.jpg'],
+    },
+    {
+        name: 'Giá Để Đàn Guitar - Chân đỡ chữ A',
+        category: 'Âm nhạc',
+        price: 99000,
+        countInStock: 500,
+        brand: 'Noname',
+        weight: 900,
+        description:
+            'Chân để đàn chữ A tiện lợi và an toàn.\nThiết kế chữ A vững chãi, có thanh chắn ngang để tăng độ bền cho chân đàn.\nChân đàn chữ A có thiết kế phù hợp cho tất cả các loại đàn guitar từ guitar acoustic đến classic lẫn guitar điện.\nSơn tĩnh điện màu đen rất sạch sẽ và sang trọng.\nChân đàn khá nhẹ và dễ dàng gấp gọn lại để phù hợp việc vận chuyển khắp nơi.\n\nCây đàn guitar của bạn sẽ không còn bị vứt lăng lóc khắp nơi nữa đâu!\nThời gian sử dụng chân đàn cực kì lâu, bạn sẽ không còn phải lo lắng về việc bảo quản đàn nữa đâu nhé!',
+        images: ['https://i.ibb.co/wy731f7/chan-do-guitar-chu-a-01.jpg'],
     },
     {
         name: 'Khóa Đàn Guitar Classic Dây Nilong',
@@ -31,10 +57,49 @@ export const music = [
         price: 80000,
         countInStock: 100,
         brand: 'Noname',
-        weight: 500,
+        weight: 300,
         description:
-            'Khóa đàn dây nilong , dùng cho đàn classic \n\nLoại khóa đài loan nhựa abs đầu vặn cao cấp , đổ nguyên khối ko sợ bể gảy \n\nHàng chất lượng tốt ',
+            'Khóa đàn dây nilong , dùng cho đàn classic \n\nLoại khóa đài loan nhựa abs đầu vặn cao cấp , đổ nguyên khối ko sợ bể gảy \n\nHàng chất lượng tốt',
         images: ['https://i.ibb.co/3kJVTcW/classic-guitar-tuner.jpg'],
+    },
+    {
+        name: 'Dán chống xước đàn guitar',
+        category: 'Âm nhạc',
+        price: 25000,
+        countInStock: 80,
+        brand: 'Noname',
+        weight: 50,
+        description:
+            'Chất liệu: Khảm trai\n\nMàu sắc: Nhiều màu\n\nCông dụng: Sản phẩm để trang trí cần đàn của bạn thêm phần cá tính, bớt nhàm chán. Có rất nhiều mẫu mã phù hợp với gu cũng như sở thích của mỗi người!',
+        images: ['https://i.ibb.co/D5pb8td/dan-chong-xuoc-dan-guitar-01.jpg'],
+    },
+    {
+        name: 'Fender CN-60S Black',
+        category: 'Âm nhạc',
+        price: 5950000,
+        countInStock: 60,
+        brand: 'Fender',
+        weight: 3350,
+        description:
+            'Fender CN-60S Classical Black giúp người trải nghiệm sự thuận tiện của dây Nylon và âm thanh của dây thép',
+        images: ['https://i.ibb.co/hCd7NCJ/fender-cn-60s-black-01.jpg'],
+    },
+    {
+        name: 'Fender Vintera Road Worn 50s Telecaster',
+        category: 'Âm nhạc',
+        price: 29400000,
+        countInStock: 10,
+        brand: 'Fender',
+        weight: 3700,
+        description:
+            "Được hoàn thiện bằng sơn mài nitrocellulose - được đánh giá cao về chất lượng âm sắc tự nhiên và các kiểu lão hóa khác biệt, sau đó được xử lý bằng tay để có vẻ ngoài chân thực, chiếc Tele® này mang đến âm thanh leng keng giống như chuông và sự thoải mái khi chơi mà bạn mong đợi từ một cây đàn guitar cổ điển tuyệt vời.\n\nĐối với âm thanh Fender hiện đại, chúng tôi đã tạo lại cặp Pickup Single Telecaster thập niên 50 sau đó khởi động mọi thứ lên một chút bằng cách tăng công suất. Twangy và rõ ràng, chúng có sức mạnh bạn cần cho bất kỳ phong cách âm nhạc nào. Cần đàn dày, có vai mập, hình chữ U 'thập niên 50' có cần đàn bằng gỗ phong 7,25 inch với 21 phím đàn cao kiểu cổ điển cho cảm giác chơi cổ điển. Bridge Tele® kiểu cổ điển với ba yên thùng bằng đồng. twang đặc trưng, ​​khoá lên dây kiểu cổ điển cung cấp tính thẩm mỹ của thời đại nguyên bản, hiệu suất mạnh mẽ và độ ổn định điều chỉnh. Các tính năng khác bao gồm nút dây đeo kiểu cổ điển, phần cứng chrome và tấm cổ bốn chốt. Bao gồm túi gig sang trọng.",
+        images: [
+            'https://i.ibb.co/Swr9vC6/fender-vintera-road-worn-50s-telecaster-01.jpg',
+            'https://i.ibb.co/9hVQswk/fender-vintera-road-worn-50s-telecaster-02.jpg',
+            'https://i.ibb.co/NtvDTtL/fender-vintera-road-worn-50s-telecaster-03.jpg',
+            'https://i.ibb.co/C1ybP36/fender-vintera-road-worn-50s-telecaster-04.jpg',
+            'https://i.ibb.co/x14K5g0/fender-vintera-road-worn-50s-telecaster-05.jpg',
+        ],
     },
     {
         name: 'Lược Đàn Guitar, Ngựa Đàn Guitar Chất Xương Già',
