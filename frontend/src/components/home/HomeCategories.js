73a0fa27de3ef252037defe8b0a415855eb002fd
gnoul_ <ttluong4869@gmail.com>
2021-12-14 03:19:25 +0700

    Modularization HomeCategories

---
+++ frontend/src/components/home/HomeCategories.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { categories } from '../../data/categories';
+
+const homeCategories = () => {
+    return (
+        <section className="container bg-white p-3">
+            <div className="fw-600 fsr-4">Danh mục</div>
+
+            <div className="d-flex flex-wrap mt-3">
+                {categories.map((item) => {
+                    return (
+                        <div key={item.id} className="category-wrapper">
+                            <div className="category-container">
+                                <Link to={item.url} className="link-inherit">
+                                    <div className="category">
+                                        <div className="category-image-container">
+                                            <img
+                                                src={item.icon}
+                                                alt={item.name}
+                                                className="category-image"
+                                            />
+                                        </div>
+                                        <div className="category-name line-clamp-2 text-center fsr-2">
+                                            {item.name}
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </section>
+    );
+};
+
+export default homeCategories;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,41 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import HomeBanner from '../components/home/HomeBanner';
-import { categories } from '../data/categories';
+import HomeCategories from '../components/home/HomeCategories';
 
 const HomePage = () => {
     return (
         <main>
             <HomeBanner />
 
-            <section className="container bg-white mt-3 p-3">
-                <div className="fw-600 fsr-4">Danh mục</div>
-
-                <div className="d-flex flex-wrap mt-3">
-                    {categories.map((item) => {
-                        return (
-                            <div key={item.id} className="category-wrapper">
-                                <div className="category-container">
-                                    <Link to={item.url} className="link-inherit">
-                                        <div className="category">
-                                            <div className="category-image-container">
-                                                <img
-                                                    src={item.icon}
-                                                    alt={item.name}
-                                                    className="category-image"
-                                                />
-                                            </div>
-                                            <div className="category-name line-clamp-2 text-center fsr-2">
-                                                {item.name}
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            </section>
+            <div className="mt-3">
+                <HomeCategories />
+            </div>
         </main>
     );
 };
