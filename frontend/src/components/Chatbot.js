e317c2310fb1ba6a1b7e76c128976b9e3b6f282c
gnoul_ <ttluong4869@gmail.com>
2021-12-16 16:22:54 +0700

    Update Chatbot and it's styling

--- frontend/src/components/Chatbot.js
+++ frontend/src/components/Chatbot.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { VscChromeClose } from 'react-icons/vsc';
 import VagabotIcon from '../images/vagabot.png';
 
 const Chatbot = () => {
@@ -6,10 +7,23 @@ const Chatbot = () => {
 
     return (
         <>
-            <div className={`chatbot-bubble ${isActivated && 'active'}`}>
+            <div
+                className={`chatbot-bubble ${isActivated && 'active'}`}
+                onClick={() => setIsActivated(true)}
+            >
                 <div className="chatbot-icon-container">
                     <img src={VagabotIcon} alt="vagabot_icon" className="chatbot-icon" />
                 </div>
+            </div>
+
+            <div className={`chat-container ${isActivated && 'active'}`}>
+                <div className="chat-header">
+                    <div className="fsr-4 text-ired fw-600">Vagabot</div>
+                    <div className="chat-exit-btn" onClick={() => setIsActivated(false)}>
+                        <VscChromeClose />
+                    </div>
+                </div>
+                <div className="chat-message-container"></div>
             </div>
         </>
     );
